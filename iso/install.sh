720fb84c78a940d95d0daba2a5217db59bbd50ab
absolute-quantum <gitcommits.mc493@aleeas.com>
2022-04-25 15:13:45 +0200

    install.sh for iso updated better

--- iso/install.sh
+++ iso/install.sh
@@ -2,14 +2,13 @@
 
 ARCHFILE="archlinux-2022.04.05-x86_64.iso"
 SIGFILE="$ARCHFILE.sig"
-REPO="archlinux"
 MIRROR="https://mirror.ams1.nl.leaseweb.net/archlinux/iso/2022.04.05"
 
-echo "Grabbing archlinux-2022.04.05-x86_64.iso"
+echo "Grabbing $MIRROR/$ARCHFILE"
 wget "$MIRROR/$ARCHFILE"
 
-echo "Grabbing $ARCHFILE.sig"
+echo "Grabbing $MIRROR/$ARCHFILE.sig"
 wget "$MIRROR/$ARCHFILE.sig"
 
-echo "GPG file verification"
+echo "Checksum verification"
 gpg --verify "$SIGFILE" "$ARCHFILE"
